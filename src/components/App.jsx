c355adb03341ff01a84e7987012ea25550409eeb
Apprentice76 <hrk.som.01@gmail.com>
2021-08-04 01:00:58 +0530

    Header & Banner components created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,36 @@
+import styles from '../styles/App.module.css'
 
 const App = () => {
-  return (
-    <div className="App">
-        
-    </div>
-  );
+	return (
+		<>
+			<div className={styles.header}>
+				<img
+					className={styles.logo_text}
+					src='img/logo-text.png'
+					alt='nf-text-logo'
+				/>
+				<img className={styles.logo} src='img/logo.png' alt='nf-logo' />
+			</div>
+			{/* header */}
+			<div className={styles.banner}>
+				<div className={styles.banner_content}>
+					<div className={styles.banner_title}>
+						Ginny &amp; Georgia
+					</div>
+					<div className={styles.banner_buttons}>
+						<div className={styles.button}>Play</div>
+						<div className={styles.button}>Add to List</div>
+					</div>
+					<div className={styles.banner_description}>
+						Angsty and awkward fifteen year old Ginny Miller often
+						feels more mature than her thirty year old mother, the
+						irresistible and dynamic Georgia Miller...
+					</div>
+				</div>
+            </div>
+            {/* banner */}
+		</>
+	)
 }
 
-export default App;
+export default App
