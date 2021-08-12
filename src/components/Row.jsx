c55472af6ea87d90bd0f08f7c5f4d928e6eb7e52
Apprentice76 <hrk.som.01@gmail.com>
2021-08-12 23:41:07 +0530

    added check for dead links

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -30,16 +30,21 @@ const Row = (props) => {
 			<h3 className={styles.row__title}>{title}</h3>
 			<div className={styles.row__poster_container}>
 				{rowData.map((movie) => (
-					<img
+                    ((large && movie.poster_path) || (!large && movie.backdrop_path)) &&
+					(<img
 						key={movie.poster_path}
-						className={large ? styles.row__poster_large : styles.row__poster}
+						className={
+							large
+								? styles.row__poster_large
+								: styles.row__poster
+						}
 						src={
 							large
 								? `https://image.tmdb.org/t/p/w342${movie.poster_path}`
 								: `https://image.tmdb.org/t/p/w300${movie.backdrop_path}`
 						}
 						alt={movie.original_title}
-					/>
+					/>)
 				))}
 			</div>
 		</div>
