84bf9362b4741f8f66e22507aeec41c8d11d873e
Apprentice76 <hrk.som.01@gmail.com>
2021-08-12 22:36:39 +0530

    cleaned up Banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,5 @@
 import styles from '../styles/Banner.module.css'
-// import component from 'styled-components'
-import bg from '../img/banner_img.jpg'
+// import component from 'styled-component'
 import { useState, useEffect } from 'react'
 import requests from '../config/requests'
 import axios from 'axios'
@@ -9,16 +8,12 @@ const Banner = () => {
 	const [loaded, setLoaded] = useState(false)
 	const [bannerData, setBannerData] = useState({})
 
-	console.log(bg)
-	const [originals, setOriginals] = useState([])
-
 	useEffect(() => {
 		axios.get(requests.fetchOriginals).then((response) => {
 			if (!response) return
 			setLoaded(true)
 
 			const movies = response.data.results
-			setOriginals(() => movies)
 
 			const rand = Math.floor(Math.random() * (movies.length - 1))
 			setBannerData(() => movies[rand])
@@ -36,15 +31,15 @@ const Banner = () => {
 	//     padding: 0 25px;
 	// 	backgroundImage:
 	// `
-	console.log(bannerData.backdrop_path)
+	// console.log(bannerData.backdrop_path)
     const bannerStyle = {
-		background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.2) 0%, rgba(10, 10, 10, 1) 90%), url('https://image.tmdb.org/t/p/original/${bannerData.backdrop_path}') no-repeat center center/cover`,
+		background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.2) 0%, rgba(10, 10, 10, 1) 90%), url('https://image.tmdb.org/t/p/w1280/${bannerData.backdrop_path}') no-repeat center center/cover`,
 		height: '60vh',
 		padding: '0 25px',
 	}
 
 	return (
-		<div className={styles.banner} style={bannerStyle}>
+		<div className={styles.banner} style={loaded ? bannerStyle: {}}>
 			{/* <BannerStyle> */}
 			<div className={styles.banner__content}>
 				<div className={styles.banner__title}>
